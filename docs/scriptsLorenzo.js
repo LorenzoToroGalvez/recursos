d704e000e4d8413d551aaa71498c78c387c36dd9
LorenzoToroGalvez <48417716+LorenzoToroGalvez@users.noreply.github.com>
2021-06-21 13:35:36 +0200

    Update scriptsLorenzo.js

--- docs/scriptsLorenzo.js
+++ docs/scriptsLorenzo.js
@@ -42,6 +42,7 @@ function Activar() {
 	let asistencia1 = document.getElementById("asistencia1");
 	let asistencia2 = document.getElementById("asistencia2");
 	let asistencia3 = document.getElementById("asistencia3");
+	let select2 = document.getElementsByClassName("select2");
 	
 	if (radioEventos.checked) {
 		titulo.disabled = false;
@@ -91,6 +92,8 @@ function Activar() {
 		asistencia1.disabled = false;
 		asistencia2.disabled = false;
 		asistencia3.disabled = false;
+		
+		select2.selectedIndex = -1;
 		
 		anioevento.disabled = true;
 		mes.disabled = true;
