13273cb9fd98a0ab46eba14433f5434c503910be
FranciscoGranados <48551529+FranciscoGranados@users.noreply.github.com>
2021-06-22 16:55:22 +0200

    Update scriptsFran.js

--- docs/scriptsFran.js
+++ docs/scriptsFran.js
@@ -4,7 +4,7 @@ function Activar() {
     let inputOrganizacion = document.getElementById("InputOrganizacion");
     let rol = selectRol.options[selectRol.selectedIndex].value;
 
-    if(rol == "Creador de Eventos"){
+    if(rol === "Creador de Eventos"){
         inputOrganizacion.disabled = false;
     }else{
         inputOrganizacion.disabled = true;
